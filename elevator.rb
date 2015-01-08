69971a96ddb73349aeae937847cfb7c7f2496e8e
Rose Weixel <rose.weixel@gmail.com>
2015-01-08 18:55:58 -0500

    finish MVP of elevator

--- elevator.rb
+++ elevator.rb
@@ -8,15 +8,36 @@ class ElevatorBank
   end
 
   def summon(floor)
-    @queue << floor
+    # implement queue later
+    # @queue << floor
+    closest_elevator(floor).go_to(floor)
+  end
+
+  def closest_elevator(floor)
+    current_distance = floors
+    chosen_elevator = nil
+    @elevators.each do |elevator|
+      if (elevator.floor - floor).abs < current_distance
+        current_distance = (elevator.floor - floor).abs
+        chosen_elevator = elevator
+      end
+    end
+    chosen_elevator
   end
 
 end
 
 class Elevator
+  attr_accessor :floor
 
-  def initialize(bank)
+  def initialize(bank, start_floor=1)
     bank.elevators << self
+    @floor = start_floor
+  end
+
+  def go_to(destination)
+    # why does this need to be an instance variable?
+    @floor = destination
   end
 
 end
--- elevator_spec.rb
+++ elevator_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'elevator.rb'
 # What is an elevator?
 # 0. there is an elevator bank that has many elevators
@@ -30,20 +31,48 @@ describe ElevatorBank do
   end
 
   describe "#summon" do
+    let(:bank) { ElevatorBank.new(7) }
+    let!(:near_elevator) { Elevator.new(bank, 2) }
+    let!(:far_elevator) { Elevator.new(bank) }
+
+    # it "should add an elevator request to the queue" do
+    #   expect{bank.summon(1)}.to change{bank.queue.count}.from(0).to(1)
+    # end
+
     it "should add an elevator request to the queue" do
-      expect{bank.summon(1)}.to change{bank.queue.count}.from(0).to(1)
+      expect{bank.summon(5)}.to change{near_elevator.floor}.from(2).to(5)
+    end
+  end
+
+  describe "#closest_elevator" do
+    it "should find the elevator closest to the request" do
+      expect(bank.closest_elevator(5)).to be_nil
+
+      near_elevator = Elevator.new(bank, 2)
+      far_elevator = Elevator.new(bank)
+
+      expect(bank.closest_elevator(5)).to eq(near_elevator)
     end
   end
 end
 
 describe Elevator do
   let(:bank) { ElevatorBank.new(7) }
+  let(:elevator) { Elevator.new(bank) }
 
-  describe ".new" do
-    it "should create an instance of Elevator" do
+  # describe ".new" do
+  #   it "should create an instance of Elevator" do
+  #     expect{
+  #       elevator = Elevator.new(bank)
+  #       }.to_not raise_error
+  #   end
+  # end
+
+  describe "#go_to" do
+    it "should change the floor to the one it was sent to" do
       expect{
-        elevator = Elevator.new(bank)
-        }.to_not raise_error
+        elevator.go_to(5)
+      }.to change{elevator.floor}.from(1).to(5)
     end
   end
 
